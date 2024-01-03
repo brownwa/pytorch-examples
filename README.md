54d0e8b36d52d4266940c7fefd320316148c3e20
Waheed Brown <waheedbrown@gmail.com>
2024-01-02 22:14:49 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# PyTorch Examples
+This repo contains examples of building, training and testing deep neural network models. The purpose is to create generative AI models locally, for use with console applications
+
+![Sample neural network output](./images/sample_neural_network_output.png)
+
+**Deep Neural Network NCHW Memory Format**
+
+![Deep Neural Network NCHW Memory Format](./images/mem_fmt_img1.png)
+
+# References
+* https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+* [Understanding Memory Formats](https://oneapi-src.github.io/oneDNN/dev_guide_understanding_memory_formats.html)
---
+++ neural_network.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+# neural_network.py
+#
+# Tuesday January 2, 2024
+# Waheed Brown
+#
+# This Python screen builds and trains a model, using a deep neural network
+# The model is then tested against the FashionMNIST dataset
+#
+# References:
+# https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+        
+def main():
+    # Download training data from open datasets.
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    # Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    batch_size = 64
+
+    # Create data loaders
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y: {y.shape} {y.dtype}")
+        break
+
+    # Get cpu, gpu or mps device for training.
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print(f"Using {device} device")
+
+    # model = NeuralNetwork.to(device)
+    nn = NeuralNetwork()
+    model = nn.to(device)
+    print(model)
+
+if __name__ == "__main__":
+    main()
